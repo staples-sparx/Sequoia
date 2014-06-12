88dddf02fa69386856bdb9ea512ca6c07443f75c
Tim Brooks <tim.brooks@staples.com>
2014-06-11 17:45:00 -0700

    Implement get path on Tree Reducer

--- src/main/java/com/stapleslabs/tree/TreeReducer.java
+++ src/main/java/com/stapleslabs/tree/TreeReducer.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class TreeReducer {
 
     public <F> void reduceTree(int root, Node<F>[] nodes, F features, Set<IFeature> missingFeatures,
-                                    List<Node<F>> subTreeNodes) {
+                               List<Node<F>> subTreeNodes) {
         TIntStack nodesToSearchStack = new TIntArrayStack();
         TIntStack parentStack = new TIntArrayStack();
         int newRoot = subTreeNodes.size();
@@ -50,5 +50,54 @@ public class TreeReducer {
             subTreeNodes.get(parentIndex).addChildOffset(subTreeNodes.size() - newRoot);
         }
         subTreeNodes.add(node.copyWithEmptyChildOffsets());
+    }
+
+    public <F> int[][] getFastPath(int root, Node<F>[] nodes, F features, Set<IFeature> missingFeatures) {
+        int[][] path = new int[nodes.length][];
+        int[] offsetState = new int[nodes.length];
+        TIntStack nodesToSearchStack = new TIntArrayStack();
+        TIntStack parentStack = new TIntArrayStack();
+
+        int currentIndex = root;
+        Node<F> node = nodes[root];
+        while (!node.isLeaf || nodesToSearchStack.size() != 0) {
+            if (node.isLeaf) {
+                modifyPath(path, offsetState, parentStack, currentIndex);
+
+                currentIndex = root + nodesToSearchStack.pop();
+                node = nodes[currentIndex];
+            } else if (missingFeatures.contains(node.feature)) {
+                if (parentStack.size() != 0) {
+                    modifyPath(path, offsetState, parentStack, currentIndex);
+                }
+
+                int[] childNodeOffsets = node.childOffsets;
+                for (int i = childNodeOffsets.length - 1; i >= 0; --i) {
+                    parentStack.push(currentIndex);
+                    nodesToSearchStack.push(childNodeOffsets[i]);
+                }
+                path[currentIndex] = childNodeOffsets;
+
+                currentIndex = root + nodesToSearchStack.pop();
+                node = nodes[currentIndex];
+            }
+            else {
+                currentIndex = root + node.nextNodeOffset(features);
+                node = nodes[currentIndex];
+            }
+        }
+        if (parentStack.size() != 0) {
+            modifyPath(path, offsetState, parentStack, currentIndex);
+        }
+
+        return path;
+    }
+
+    private void modifyPath(final int[][] path, final int[] offsetState, final TIntStack parentStack, final int currentIndex) {
+        int parentIndex = parentStack.pop();
+        int offsetIndex = offsetState[parentIndex];
+        ++offsetState[parentIndex];
+        int[] childOffsets = path[parentIndex];
+        childOffsets[offsetIndex] = currentIndex - childOffsets[offsetIndex];
     }
 }
---
+++ src/test/java/com/stapleslabs/tree/TreeReducerTest.java
@@ -0,0 +1,64 @@
+package com.stapleslabs.tree;
+
+import com.stapleslabs.features.IFeature;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by timbrooks on 6/11/14.
+ */
+public class TreeReducerTest {
+
+    @Test
+    public void testGetFastPath() {
+        int[] node1Offsets = {1, 2};
+        int[] node3Offsets = {3, 4};
+        int[] node4Offsets = {4, 5};
+        Node<Map<IFeature, Integer>> node0 = new Node<>(Feature.COST, -1.0, false, node1Offsets, new DummyCondition());
+        Node<Map<IFeature, Integer>> node1 = new Node<>(null, 25.0, true, null, null);
+        Node<Map<IFeature, Integer>> node2 = new Node<>(Feature.MISSING, -1.0, false, node3Offsets, new DummyCondition());
+        Node<Map<IFeature, Integer>> node3 = new Node<>(Feature.COST, -1.0, false, node4Offsets, new DummyCondition());
+        Node<Map<IFeature, Integer>> node4 = new Node<>(null, 35.0, true, null, null);
+        Node<Map<IFeature, Integer>> node5 = new Node<>(null, 15.0, true, null, null);
+
+
+        Tree<Map<IFeature, Integer>> tree = new Tree<>(Arrays.asList(node0, node1, node2, node3, node4, node5));
+        TreeReducer treeReducer = new TreeReducer();
+
+        HashMap<IFeature, Integer> features = new HashMap<>();
+        features.put(Feature.COST, 10);
+
+        HashSet<IFeature> missingFeatures = new HashSet<>();
+        missingFeatures.add(Feature.MISSING);
+
+        int[][] fastPath = treeReducer.getFastPath(0, tree.getNodes(), features, missingFeatures);
+
+        System.out.println(Arrays.toString(fastPath));
+        int[][] expectedFastPath = {null, null, {2, 0}, null, null, null};
+        assertArrayEquals(expectedFastPath, fastPath);
+    }
+
+    private enum Feature implements IFeature {
+        COST,
+        MISSING
+    }
+
+    private class DummyCondition implements ICondition<Map<IFeature, Integer>> {
+
+        @Override
+        public int nextOffsetIndex(final IFeature feature, final Map<IFeature, Integer> features) {
+            if (features.get(feature) > 5) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}

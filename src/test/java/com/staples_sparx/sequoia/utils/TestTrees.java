66f42fbdd895c180fa64f5e68997d6889c350b30
Tim Brooks <tim.brooks@staples.com>
2015-06-28 16:16:36 -0500

    Add license

--- src/test/java/com/staples_sparx/sequoia/utils/TestTrees.java
+++ src/test/java/com/staples_sparx/sequoia/utils/TestTrees.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2014 Staples Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.staples_sparx.sequoia.utils;
 
 import com.staples_sparx.sequoia.Condition;
@@ -7,9 +24,6 @@ import com.staples_sparx.sequoia.Tree;
 
 import java.util.*;
 
-/**
- * Created by timbrooks on 5/15/14.
- */
 public class TestTrees {
 
     private static final Map<Integer, NodeBlueprint> branchNodeRecipes;
